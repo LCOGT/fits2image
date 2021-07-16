358c6915fd715d785ea883138253d7a3eae5f3f6
Mark Bowman <mbowman@lco.global>
2021-07-16 12:26:38 -0700

    Bump verison to 0.4.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ DESCRIPTION = """Common libraries for the conversion and scaling of fits images"
 
 setup(
     name="fits2image",
-    version="0.4.3",
+    version="0.4.4",
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     author='Jon Nation',
